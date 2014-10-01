f825c4500494bf16d324c96f80731045de61acf7
Dhiraj Rajendra Jadhao <dhirajjadhao@gmail.com>
2014-10-02 01:12:07 +0200

    TAH RGB App optimised for iPad

--- TAH RGB/TAH RGB/HomeViewController.h
+++ TAH RGB/TAH RGB/HomeViewController.h
@@ -1,71 +1,33 @@
 //
 //  HomeViewController.h
-//  TAH RGB
+//  TAH Sensor
 //
-//  Created by Dhiraj on 17/07/14.
+//  Created by Dhiraj on 05/07/14.
 //  Copyright (c) 2014 dhirajjadhao. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "TAHble.h"
-
-
+#import "AppDelegate.h"
 
 @class CBPeripheral;
 @class TAHble;
 
-@interface HomeViewController : UIViewController<BTSmartSensorDelegate>
+@interface HomeViewController : UIViewController<UICollectionViewDataSource, UICollectionViewDelegate,BTSmartSensorDelegate>
 {
+    NSArray *arrayOfdescription;
+    NSArray *arrayofImages;
+    NSArray *arrayofCellIdentifiers;
 
-
-
-    
-    
-    IBOutlet UIButton *redcol;
-    IBOutlet UIButton *bluwber;
-    IBOutlet UIButton *yellow;
-    IBOutlet UIButton *greencol;
-    IBOutlet UIButton *purple;
-    IBOutlet UIButton *orange;
-    IBOutlet UIButton *lime;
-    IBOutlet UIButton *sky;
-    
-    IBOutlet UIButton *pink;
-    IBOutlet UIButton *maroon;
-    IBOutlet UIButton *darkblue;
-    IBOutlet UIButton *bluebean;
-    
-    
-    IBOutlet UIButton *rainbow;
-    IBOutlet UIButton *off;
     IBOutlet UILabel *ConnectionStatusLabel;
     
-
 }
-
-
 
 @property (strong, nonatomic) CBPeripheral *peripheral;
 @property (strong, nonatomic) TAHble *sensor;
 
 
-- (IBAction)redcol:(id)sender;
-- (IBAction)blueber:(id)sender;
-- (IBAction)yellow:(id)sender;
-- (IBAction)greencol:(id)sender;
-- (IBAction)purple:(id)sender;
-- (IBAction)orange:(id)sender;
-- (IBAction)lime:(id)sender;
-- (IBAction)sky:(id)sender;
-
-- (IBAction)off:(id)sender;
-- (IBAction)rainbow:(id)sender;
-
-- (IBAction)pink:(id)sender;
-- (IBAction)maroon:(id)sender;
-- (IBAction)darkblue:(id)sender;
-- (IBAction)bluebean:(id)sender;
-
+@property (weak, nonatomic) IBOutlet UICollectionView *myCollectionView;
 
 
 @end
--- TAH RGB/TAH RGB/HomeViewController.m
+++ TAH RGB/TAH RGB/HomeViewController.m
@@ -1,14 +1,23 @@
 //
 //  HomeViewController.m
-//  TAH RGB
+//  TAH
 //
-//  Created by Dhiraj on 17/07/14.
+//  Created by Dhiraj on 05/07/14.
 //  Copyright (c) 2014 dhirajjadhao. All rights reserved.
 //
 
-#import "HomeViewController.h"
-#import "TAHble.h"
 #import <AudioToolbox/AudioToolbox.h>
+#import "HomeViewController.h"
+#import "TAHbleTableController.h"
+#import "TAHble.h"
+#import "CustomCell.h"
+
+#import "RGBViewController.h"
+#import "SettingsViewController.h"
+#import "SocialViewController.h"
+#import "MediaViewController.h"
+
+
 
 @interface HomeViewController ()
 
@@ -16,25 +25,46 @@
 
 @implementation HomeViewController
 
-@synthesize sensor;
 @synthesize peripheral;
+@synthesize sensor;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
     
+
     // Settings Up Sensor Delegate
-    self.sensor.delegate = self;
+     self.sensor.delegate = self;
+    
+    
+    
+    /////////////// Setting Collection View Delegate and Datasource /////////////
+    
+    [[self myCollectionView]setDelegate:self];
+    [[self myCollectionView]setDataSource:self];
+    ////////////////////////////////////////////////////////////////////////////
+    
+    
+    /////////////// Setting array of Images and Identifiers for Collection View Cells /////////////
+    arrayofImages = [[NSArray alloc]initWithObjects:@"frame.png",@"frame.png",@"frame.png",@"frame.png",@"frame.png",@"frame.png",@"frame.png",@"frame.png",nil];
+    arrayofCellIdentifiers = [[NSArray alloc]initWithObjects:@"Cell1",@"Cell2",@"Cell3",@"Cell4",@"Cell5",@"Cell6", nil];
+    
+    ////////////////////////////////////////////////////////////////////////////
     
     // Set Connection Status Image
     [self UpdateConnectionStatusLabel];
-}
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
 
@@ -45,22 +75,149 @@
 }
 
 
-///////////// Update Device Connection Status Image //////////
+    ///////////// Update Device Connection Status Image //////////
 -(void)UpdateConnectionStatusLabel
 {
     
     
     if (sensor.activePeripheral.state)
     {
-        
+
         ConnectionStatusLabel.backgroundColor = [UIColor colorWithRed:128.0/255.0 green:255.0/255.0 blue:0.0/255.0 alpha:1.0];
     }
     else
     {
-        
+
         ConnectionStatusLabel.backgroundColor = [UIColor colorWithRed:255.0/255.0 green:128.0/255.0 blue:0.0/255.0 alpha:1.0];
     }
 }
+
+
+
+///////////// Collection View Setup /////////////////
+
+-(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+-(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    
+    return 4;
+    
+}
+
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    //CustomCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell1" forIndexPath:indexPath];
+    
+    CustomCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:[arrayofCellIdentifiers objectAtIndex:indexPath.row] forIndexPath:indexPath];
+    
+    [[cell myImage]setImage:[UIImage imageNamed:[arrayofImages objectAtIndex:indexPath.row]]];
+    
+    
+    
+    return cell;
+}
+
+//////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+
+
+//////////////////////// Preparing Segue for Navigation //////////////////////////
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Changes title of the Back Button in destintion controller
+    UIBarButtonItem *newBackButton =
+    [[UIBarButtonItem alloc] initWithTitle:@"Home"
+                                     style:UIBarButtonItemStyleBordered
+                                    target:nil
+                                    action:nil];
+    [[self navigationItem] setBackBarButtonItem:newBackButton];
+    
+    
+    
+    if ([[segue identifier] isEqualToString:@"Cell1"])
+    {
+        RGBViewController *detailViewController = [segue destinationViewController];
+        
+        detailViewController.sensor = self.sensor;
+        
+        detailViewController.title = @"RGB";
+        
+        
+        
+    }
+    
+
+    
+    else if ([[segue identifier] isEqualToString:@"Cell2"])
+    {
+        SettingsViewController *detailViewController = [segue destinationViewController];
+        
+        detailViewController.sensor = self.sensor;
+        
+        detailViewController.title = @"SETTINGS";
+    }
+    
+    
+    else if ([[segue identifier] isEqualToString:@"Cell3"])
+    {
+        SocialViewController *detailViewController = [segue destinationViewController];
+        
+        detailViewController.sensor = self.sensor;
+        
+        detailViewController.title = @"Share";
+        
+    }
+    
+    
+    else if ([[segue identifier] isEqualToString:@"Cell4"])
+    {
+        MediaViewController *detailViewController = [segue destinationViewController];
+        
+        detailViewController.sensor = self.sensor;
+        
+        detailViewController.title = @"YouTube";
+    }
+
+    
+    
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
 
 
 
@@ -72,6 +229,7 @@
     
     NSLog(@"%@",value);
 }
+
 
 
 
@@ -88,138 +246,20 @@
     [sensor disconnect:sensor.activePeripheral];
     
     NSLog(@"TAH Device Disconnected");
-    
+
     
     //////// Local Alert Settings
     
     AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
-    
+
     /////////////////////////////////////////////
     
     // Set Connection Status Image
     [self UpdateConnectionStatusLabel];
     
-    
+
 }
 
 
-
-- (IBAction)redcol:(id)sender
-{
-      NSString *command;
-      command = @"255,0,0,1R";
-      NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-     [sensor write:sensor.activePeripheral data:feed];
-    
-}
-
-- (IBAction)blueber:(id)sender
-{
-    NSString *command;
-    command = @"0,0,255,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-
-
-- (IBAction)yellow:(id)sender
-{
-    NSString *command;
-    command = @"255,255,0,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-
-
-- (IBAction)greencol:(id)sender
-{
-    NSString *command;
-    command = @"0,255,0,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-
-
-- (IBAction)purple:(id)sender
-{
-    NSString *command;
-    command = @"64,0,128,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-
-
-- (IBAction)orange:(id)sender
-{
-    NSString *command;
-    command = @"255,154,0,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-
-
-- (IBAction)lime:(id)sender
-{
-    NSString *command;
-    command = @"128,255,0,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-
-
-- (IBAction)sky:(id)sender
-{
-    NSString *command;
-    command = @"102,204,255,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-
-- (IBAction)off:(id)sender
-{
-    
-    NSString *command;
-    command = @"0,0,0,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-
-- (IBAction)rainbow:(id)sender {
-    
-    NSString *command;
-    command = @"255,255,255,2R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-
-
-- (IBAction)pink:(id)sender
-{
-    NSString *command;
-    command = @"255,0,125,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-- (IBAction)maroon:(id)sender
-{
-    NSString *command;
-    command = @"131,0,62,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-- (IBAction)darkblue:(id)sender
-{
-    NSString *command;
-    command = @"0,128,127,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
-- (IBAction)bluebean:(id)sender
-{
-    NSString *command;
-    command = @"255,255,255,1R";
-    NSData *feed = [command dataUsingEncoding:[NSString defaultCStringEncoding]];
-    [sensor write:sensor.activePeripheral data:feed];
-}
 
 @end
